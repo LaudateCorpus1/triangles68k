97bfbf7f33481aa91000ace365f18a8b2a03111f
veevyo <41368076+veevyo@users.noreply.github.com>
2021-03-30 17:12:31 -0500

    Added user input

--- triangles/triangles.c
+++ triangles/triangles.c
@@ -3,14 +3,42 @@
 void _main(void) {
 	int x1, y1, x2, y2, x3, y3;
 	int i;
+	int val;
 	randomize();
 	ClrScr();
-	DrawStr(0, 0, "Press any key to generate", A_NORMAL);
-	DrawStr(0, 10, "3 random triangles.", A_NORMAL);
-	DrawStr(0, 20, "Press ESC to quit.", A_NORMAL);
+	DrawStr(0, 0, "Enter a number (1-5) to", A_NORMAL);
+	DrawStr(0, 10, "create that many triangles.", A_NORMAL);
+	DrawStr(0, 20, "After you enter a number,", A_NORMAL);
+	DrawStr(0, 30, "you can continue to create", A_NORMAL);
+	DrawStr(0, 40, "triangles by pressing", A_NORMAL);
+	DrawStr(0, 50, "any key. Press ESC to quit.", A_NORMAL);
+	switch (ngetchx()) {
+		case 49:
+			val = 1;
+			break;
+		case 50:
+			val = 2;
+			break;
+		case 51:
+			val = 3;
+			break;
+		case 52:
+			val = 4;
+			break;
+		case 53:
+			val = 5;
+			break;
+		default:
+			ClrScr();
+			DrawStr(0, 0, "You did not enter", A_NORMAL);
+			DrawStr(0, 10, "a valid number.", A_NORMAL);
+			DrawStr(0, 20, "3 triangles will generate.", A_NORMAL);
+			val = 3;
+	}
+	
 	while (ngetchx() != KEY_ESC) {
 		ClrScr();
-		for (i = 0; i < 3; i++) {
+		for (i = 0; i < val; i++) {
 			x1 = random(LCD_WIDTH);
 			y1 = random(LCD_HEIGHT);
 			x2 = random(LCD_WIDTH);
